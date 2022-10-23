f1890dcdebad6faa7703c5f4bdf4f0a22d79ee9a
Saros72 <48892226+Saros72@users.noreply.github.com>
2022-10-23 16:37:05 +0200

    Add files via upload

--- generator.py
+++ generator.py
@@ -2,58 +2,6 @@
 
 #v2.19.0
 
-# Nastavení
-# Počet dní (1-15)
-days = 3
-
-# Počet dní zpětně (0-7)
-days_back = 1
-
-# Výběr zdroje kanálů
-# 1 = povolit
-# 0 = zakázat
-TV_SMS_CZ = 1
-T_MOBILE_TV_GO = 1
-MAGIO_GO = 1
-O2_TV_SPORT = 1
-MUJ_TV_PROGRAM_CZ = 1
-SLEDOVANITV_CZ = 1
-SLEDOVANIETV_SK = 1
-TV_SPIEL = 1
-OTT_PLAY = 1
-
-# Seznam vlastních kanálů
-# Seznam id kanálů oddělené čárkou (např.: "2,3,32,94")
-# Pro všechny kanály ponechte prázdné
-TV_SMS_CZ_IDS = ""
-T_MOBILE_TV_GO_IDS = ""
-MAGIO_GO_IDS = ""
-O2_TV_IDS = ""
-MUJ_TV_PROGRAM_IDS = ""
-SLEDOVANI_TV_CZ_IDS = ""
-SLEDOVANIE_TV_SK_IDS = ""
-TV_SPIEL_IDS = ""
-OTT_PLAY_IDS = ""
-
-
-#Nahrát EPG na ftp server
-#Ano = 1
-#Ne = 0
-ftp_upload = 0
-ftp_server = ""
-ftp_port = 21
-ftp_login = ""
-ftp_password = ""
-ftp_folder = "/"
-
-#Auto aktualizace
-#Ano = 1
-#Ne = 0
-update = 0
-#Každých x hodin
-interval = 12
-
-
 import logging
 logging.basicConfig(filename='log.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 try:
@@ -70,6 +18,7 @@ try:
     import time
     import schedule
     from bs4 import BeautifulSoup
+    from settings import*
 except Exception as ex:
     print(ex)
     logging.error("365 EPG Generator - %s" % ex)
@@ -78,7 +27,7 @@ except Exception as ex:
 
 
 dn = os.path.dirname(os.path.realpath(__file__))
-fn = os.path.join(dn,"epg.xml")
+fn = os.path.join(dn,file_name)
 custom_names_path = os.path.join(dn,"custom_names.txt")
 now = datetime.now()
 local_now = now.astimezone()
---
+++ settings.py
@@ -0,0 +1,52 @@
+# Nastavení
+# Počet dní (1-15)
+days = 3
+
+# Počet dní zpětně (0-7)
+days_back = 1
+
+# Výběr zdroje kanálů
+# 1 = povolit
+# 0 = zakázat
+TV_SMS_CZ = 1
+T_MOBILE_TV_GO = 1
+MAGIO_GO = 1
+O2_TV_SPORT = 1
+MUJ_TV_PROGRAM_CZ = 1
+SLEDOVANITV_CZ = 1
+SLEDOVANIETV_SK = 1
+TV_SPIEL = 1
+OTT_PLAY = 1
+
+# Seznam vlastních kanálů
+# Seznam id kanálů oddělené čárkou (např.: "2,3,32,94")
+# Pro všechny kanály ponechte prázdné
+TV_SMS_CZ_IDS = ""
+T_MOBILE_TV_GO_IDS = ""
+MAGIO_GO_IDS = ""
+O2_TV_IDS = ""
+MUJ_TV_PROGRAM_IDS = ""
+SLEDOVANI_TV_CZ_IDS = ""
+SLEDOVANIE_TV_SK_IDS = ""
+TV_SPIEL_IDS = ""
+OTT_PLAY_IDS = ""
+
+# název souboru
+file_name = "epg.xml"
+
+#Nahrát EPG na ftp server
+#Ano = 1
+#Ne = 0
+ftp_upload = 0
+ftp_server = ""
+ftp_port = 21
+ftp_login = ""
+ftp_password = ""
+ftp_folder = "/"
+
+#Auto aktualizace
+#Ano = 1
+#Ne = 0
+update = 0
+#Každých x hodin
+interval = 12
